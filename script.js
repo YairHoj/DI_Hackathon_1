070f605298d479e9a5c7fe01d96acbbdf7582d2d
Thecoder03 <amdurzachary@gmail.com>
2022-10-17 23:41:53 +0300

    Work on the two popup buttons

--- script.js
+++ script.js
@@ -1,40 +1,69 @@
 //PopUp box for new tab
 let newTabButton = document.getElementById("tab-button");
 
-newTabButton.addEventListener("click", TabPopUp);
-
+let tabPopUp;
+let isTabBoxPopUpActive;
 function TabPopUp() {
   console.log("this event worked");
-  let popUp = document.getElementById("newTabBoxPopUpHidden");
+  popUp = document.getElementById("newTabBoxPopUpHidden");
   popUp.removeAttribute("id");
   popUp.classList.add("newTabBoxPopUp");
+  isTabBoxPopUpActive = true;
+}
+newTabButton.addEventListener("click", TabPopUp);
+//Exit PopUp Tab
+
+function ExitTabPopUp() {
+  popUp.removeAttribute("class");
+  popUp.setAttribute("id", "newTabBoxPopUpHidden");
 }
 
-// Take input from New Tab title and Append the value to the location select in NewBox to allow ToDo items in certain locations
+//Take input from New Tab title and Append the value to the location select in NewBox to allow ToDo items in certain locations
 $(document).ready(function () {
   $(document).on("submit", "#newTabForm", function () {
     let TabTitleInput = document.getElementById("newTabForm").elements[0].value;
     let selectForm = document.getElementById("newBox-Location");
-    console.log(TabTitleInput);
     let newSelect = document.createElement("option");
     newSelect.textContent = TabTitleInput;
     selectForm.append(newSelect);
+    ExitTabPopUp();
     return false;
   });
 });
 
-//Create a new Tab
-function CreateTab() {
-  let newTab = document.createElement("div");
-}
+//Create a New tab
+$(document).ready(function () {
+  $(document).on("submit", "#newTabForm", function () {
+    let newTab = document.createElement("div");
+    return false;
+  });
+});
 
 //popup and dissapear function for new todo box
 let newBoxButton = document.getElementById("box-button");
-
+let isToDoBoxPopUpActive;
 newBoxButton.addEventListener("click", boxPopUp);
 
 function boxPopUp() {
   let popUp = document.getElementById("newBoxPopUpHidden");
   popUp.removeAttribute("id");
   popUp.classList.add("newBoxPopUp");
+  isToDoBoxPopUpActive = true;
 }
+
+// Exit button for new todo box
+let toDoBoxPopUp;
+toDoBoxPopUp = document.getElementById("newBoxPopUpHidden");
+
+function ExitBoxPopUp() {
+  toDoBoxPopUp.removeAttribute("class");
+  toDoBoxPopUp.setAttribute("id", "newBoxPopUpHidden");
+}
+
+//Create a TodoBox
+$(document).ready(function () {
+  $(document).on("submit", "#newToDoBox", function () {
+    ExitBoxPopUp();
+    return false;
+  });
+});
