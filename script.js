fcc258a2b6dc8153d54776af3b492f8cf4aa7d63
YairHoj <yairhojbergcoding@gmail.com>
2022-10-17 21:16:50 +0300

    Revamped design + Dropdown buttons + title design

--- script.js
+++ script.js
@@ -29,15 +29,15 @@ function boxPopUp() {
 
 var coll = document.getElementsByClassName("collapsible");
 var i;
-
+var svg = document.getElementsByClassName("collapsible-image");
 for (i = 0; i < coll.length; i++) {
   coll[i].addEventListener("click", function () {
     this.classList.toggle("active");
     var content = this.nextElementSibling;
-    if (content.style.display === "block") {
-      content.style.display = "none";
+    if (content.style.maxHeight) {
+      content.style.maxHeight = null;
     } else {
-      content.style.display = "block";
+      content.style.maxHeight = content.scrollHeight + "1px";
     }
   });
 }
